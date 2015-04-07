a2429499b736c350172775110e41e2ede5a64ecf
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2015-04-07 15:23:10 +0530

    Minor changes

--- RestSampleApplication/src/com/sample/rest/SendMessage.java
+++ RestSampleApplication/src/com/sample/rest/SendMessage.java
@@ -4,10 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.ext.Provider;
 
 import org.glassfish.jersey.server.ResourceConfig;
