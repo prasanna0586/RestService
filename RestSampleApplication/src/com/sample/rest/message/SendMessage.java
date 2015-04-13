766f53f9374c2bda03a0d1d54f92fe3b888061dd
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2015-04-13 15:52:18 +0530

    Added Arithmetic service and configured security constraints in web.xml

--- RestSampleApplication/src/com/sample/rest/message/SendMessage.java
+++ RestSampleApplication/src/com/sample/rest/message/SendMessage.java
@@ -1,8 +1,7 @@
-package com.sample.rest;
+package com.sample.rest.message;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
